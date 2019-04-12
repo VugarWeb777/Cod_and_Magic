90620dc9ae9309643759011d9e10138516626286
Вугар <vugarguliev333@gamil.com>
2019-04-12 17:22:45 +0300

    refactoring

--- js/render-wizards.js
+++ js/render-wizards.js
@@ -1,15 +1,50 @@
+/* eslint-disable no-console */
 "use strict";
 
 (function () {
-	document.querySelector(".setup-similar").classList.remove("hidden");
-	window.similarListElement = document.querySelector(".setup-similar-list");
-	window.similarWizardTemplete = document.querySelector("#similar-wizard-template").content.querySelector(".setup-similar-item");
+	var userDialog =  document.querySelector(".setup-similar");
+	userDialog.classList.remove("hidden");
 
-	window.renderWizard = function (wizard) {
-		var wizardElement = window.similarWizardTemplete.cloneNode(true);
+	var similarListElement = document.querySelector(".setup-similar-list");
+	var similarWizardTemplete = document.querySelector("#similar-wizard-template").content.querySelector(".setup-similar-item");
+
+	function createWizards () {
+		for (var i = 0; i <4; i++) {
+			var wizardObj = new window.wizards.obj(window.wizards.getName(),window.wizards.getCoatColor(),window.wizards.getEyesColor());
+			window.wizards.wizardsList.push(wizardObj);
+		}
+		return window.wizards.wizardsList;
+	}
+
+	createWizards();
+
+
+	var renderWizard = function (wizard) {
+		var wizardElement = similarWizardTemplete.cloneNode(true);
 		wizardElement.querySelector(".setup-similar-label").textContent = wizard.wizardName;
 		wizardElement.querySelector(".wizard-coat").style.fill = wizard.coatColor;
 		wizardElement.querySelector(".wizard-eyes").style.fill = wizard.eyesColor;
 		return wizardElement;
 	};
+
+	function append() {
+		var fragment = document.createDocumentFragment();
+		for (var i = 0; i < window.wizards.wizardsList.length; i++) {
+			fragment.appendChild(renderWizard(window.wizards.wizardsList[i]));
+		}
+		similarListElement.appendChild(fragment);
+	}
+
+	append();
+
+
+	/*Отправка формы ajax*/
+	/*var form = document.forms["wizard-form"];
+
+	form.addEventListener("submit",function (evt) {
+		window.upload(new FormData(form),function () {
+			userDialog.classList.add("hidden");
+		});
+		evt.preventDefault();
+	});*/
 })();
